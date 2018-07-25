c8ae941ea409a49257f07bb433961e1161b5fc33
Ankur <rathore.ankur@gmail.com>
2018-07-25 17:01:49 +0530

    added the api viewset

---
+++ nokeep/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.urls import path
+from rest_framework import routers
+
+from .views_api import NoteViewset
+
+router = routers.DefaultRouter()
+router.register('notes', NoteViewset)
+
+urlpatterns = [
+    path("^", include(router.urls)),
+]
